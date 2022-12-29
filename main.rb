a8c94a83d196bef366dc64b5e9ef4c3ed9f0c3ac
pndesu <tranvanphucnguyen1@gmail.com>
2022-12-30 01:28:40 +1100

    Finish basic gameplay

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class Game
     attr_accessor :code, :hiddenCode
     def play()
@@ -8,14 +10,36 @@ class Game
     
     def setCode(wordList)
         @code = wordList.sample
-        puts @hiddenCode = Array.new(@code.length, '_').join
+        puts "Your random word has been choosen. It has #{@code.length} characters.".blue
+        @hiddenCode = Array.new(@code.length, '_').join("")
+        puts @hiddenCode.blue
+        @lives = 8
+        @guessed = []
         playerGuess()
     end
 
+    def displayTurnPrompt()
+        <<~HEREDOC
+        
+        Your turn to guess one letter in the secret word.
+        You can also type 'save' or 'exit' to leave the game.
+        You have #{@lives} lives left.
+
+        HEREDOC
+    end
+
     def playerGuess()
-        guess = gets.chomp
+        puts displayTurnPrompt()
+        print "Your guess: "
+        guess = gets.chomp.downcase
         if (guess.match(/^[a-zA-Z]/))
-            compare(code, guess.downcase)
+            if (guess.length != 1)
+                guess = checkLength(guess)
+            end
+            if (@guessed.include?(guess))
+                guess = checkRepeated(guess)
+            end
+            compare(code, guess)
         else
             playerGuess()
         end
@@ -28,16 +52,20 @@ class Game
     end
 
     def updateCode(indexes, guess)
+        @guessed << guess
         if (indexes.length > 0)
             indexes.each do |i|
-                @hiddenCode = @hiddenCode.split("")
                 @hiddenCode[i] = guess
             end
-            puts @hiddenCode = @hiddenCode.join("")
+            puts @hiddenCode.blue + " " + @guessed.join(" ")
             checkRemaining(@hiddenCode)
         else
-            puts "No character found!"
-            playerGuess()
+            puts "Sorry, #{guess} is not in the secret word."
+            puts ""
+            puts @hiddenCode.blue + " " + @guessed.join(" ")
+            puts ""
+            @lives = updateLives(@lives)
+            playerGuess() if (@lives > 0)
         end
     end
 
@@ -45,14 +73,37 @@ class Game
         if (hiddenCode.include?('_'))
             playerGuess()
         else
-            puts "Secret word found!"
+            puts "Congratulation! You found the secret word!".blue
         end
+    end
+
+    def checkRepeated(guess)
+        loop do
+            print "You have guessed #{guess}. Please enter another character: ".yellow
+            input = gets.chomp
+            if (!@guessed.include?(input))
+                return input.downcase
+            end
+        end
+    end
+
+    def checkLength(guess)
+        loop do
+            print "Please enter one character [a-z]: ".yellow
+            input = gets.chomp
+            if (input.length == 1 && input.match(/^[a-zA-Z]/))
+                return input.downcase
+            end
+        end
+    end
+
+    def updateLives(lives)
+        lives -= 1
+        if (lives == 0)
+            puts "You were beaten".red
+        end
+        lives
     end
 end
 
 Game.new.play
-
-
-
-
-
--- tempCodeRunnerFile.rb
+++
@@ -1,2 +0,0 @@
-s = "a#asg#sdfg#d##"
-p a = (0 ... s.length).find_all { |i| s[i,1] == '#' }
